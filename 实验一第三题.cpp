689cfc8f947b83bd363057c8b93239ef792793e3
PinelliaCSU <3218346707@qq.com>
2024-11-04 20:40:58 +0800

    Update 实验一第三题.cpp
    这个原本写在vs上忘记移交了

--- 实验一第三题.cpp
+++ 实验一第三题.cpp
@@ -7,4 +7,14 @@ int main()
 	cout << " double length:" << sizeof(double) << endl;
 	cout << "float length:" << sizeof(float) << endl;
 	cout << "long length:" << sizeof(long) << endl;
+	cout << "long length:" << sizeof(long) << endl;
+        cout << "long long length:" << sizeof(long long) << endl;
+        cout << "short length:" << sizeof(short) << endl;
+	cout << "bool length:" << sizeof(bool) << endl;
+	cout << "wchar_t length:" << sizeof(wchar_t) << endl;
+	cout << "char16_t length:" << sizeof(char16_t) << endl;
+        cout << "char32_t length:" << sizeof(char32_t) << endl;
+	//错误的	cout << "void length:" << sizeof(void) << endl;
+        cout << "signed length:" << sizeof(signed) << endl;
+	return 0;
 }
